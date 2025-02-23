70187d2a43a9b1eb56850a6d0a7e30fade4639b9
Victor Wu <victorwu820@gmail.com>
2025-02-22 19:58:13 -0500

    Change the background to yellow

--- mentor-ai/components/background-wave.tsx
+++ mentor-ai/components/background-wave.tsx
@@ -9,7 +9,7 @@ export const BackgroundWave = () => {
       muted
       loop
       controls={false}
-      className="fixed grayscale object-cover bottom-0 z-[-1] hidden md:block pointer-events-none opacity-75 "
+      className="fixed [filter:sepia(100%)_saturate(400%)_brightness(95%)_hue-rotate(15deg)] object-cover bottom-0 z-[-1] hidden md:block pointer-events-none opacity-75"
     />
   );
 };
